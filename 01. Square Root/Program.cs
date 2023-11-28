f6ba072db7d619e02ee8c3eb7b41ea58f8449e52
Mmitov270 <Mmitov270@DESKTOP-QAOS3OF>
2023-11-28 20:59:44 +0200

    Add project files.

---
+++ 01. Square Root/Program.cs
@@ -0,0 +1,15 @@
+﻿int num = int.Parse(Console.ReadLine());
+try
+{
+       if (num < 0)
+    {
+        throw new Exception();
+    }
+    double sqrt = Math.Sqrt(num);
+    Console.WriteLine(sqrt);
+}
+catch
+{
+    Console.WriteLine("Invalid number.");
+}
+Console.WriteLine("Goodbye.");
